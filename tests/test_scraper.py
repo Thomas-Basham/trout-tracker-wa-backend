dfbafd52bdae7faa3df42e244fb6f6946f054511
Thomas Basham <Bashamtg@gmail.com>
2023-03-13 13:56:59 -0700

    updated scraper tests

--- tests/test_scraper.py
+++ tests/test_scraper.py
@@ -6,19 +6,19 @@ from scraper import scrape_date, scrape_lake_names, scrape_derby_names, scrape_s
 
 
 def test_scrape_lake_names():
-    assert scrape_lake_names()
+    assert len(scrape_lake_names()) > 20 and type(scrape_lake_names()) == list
 
 
 def test_scrape_stock_count():
-    assert scrape_stock_count()
+    assert len(scrape_stock_count()) > 20 and type(scrape_stock_count()) == list
 
 
 def test_scrape_date():
-    assert scrape_date()
+    assert len(scrape_date()) > 20 and type(scrape_date()) == list
 
 
 def test_scrape_derby_names():
-    assert scrape_derby_names()
+    assert type(scrape_derby_names()) == list
 
 
 @pytest.mark.skip('Skipped for Github Test Badge')
@@ -32,7 +32,6 @@ def test_geocoder():
     load_dotenv()
     locator = GoogleV3(api_key=os.getenv('GV3_API_KEY'))
     lat_lon = locator.geocode('BLUE Lake Columbia County Washington').point
-    print(lat_lon)
     assert lat_lon == (46.2775138, -117.814262, 0.0)
 
 
