8c27c08b2ef0380ef0c5a3ee2bc605da375b1705
Thomas Basham <Bashamtg@gmail.com>
2023-03-14 14:10:07 -0700

    refactored to create a seperate class for the database

--- scraper.py
+++ scraper.py
@@ -1,16 +1,18 @@
 # from geopy.geocoders import Nominatim
-import requests
+import re
+import os
 import time
+import requests
+from datetime import datetime
 from bs4 import BeautifulSoup
 from geopy import GoogleV3
-import re
 from dotenv import load_dotenv
-import os
-from sqlalchemy import create_engine, Column, Integer, String, Boolean, update
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine, func, text, Column, Integer, String, Boolean, DateTime
 
 load_dotenv()
+
 # Create a SQLAlchemy base
 Base = declarative_base()
 
@@ -20,12 +22,20 @@ class StockedLakes(Base):
   __tablename__ = 'stocked_lakes_table'
   id = Column(Integer, primary_key=True)
   lake = Column(String)
-  stocked_fish = Column(String)
-  date = Column(String)
+  stocked_fish = Column(Integer)
+  date = Column(DateTime)
   latitude = Column(String)
   longitude = Column(String)
   directions = Column(String)
   derby_participant = Column(Boolean)
+
+  # @property
+  # def date(self):
+  #   return datetime.strptime(self.date, '%b %d, %Y') or datetime.strptime(self.date, '%b %dd, %Y')
+  #
+  # @date.setter
+  # def date(self, value):
+  #   self.date = datetime.strptime(value, '%m/%d/%Y').strftime('%b %d, %Y')
 
 
 # Create the derby_lakes_table class
@@ -35,186 +45,212 @@ class DerbyLake(Base):
   lake = Column(String)
 
 
-# Load Database
-if os.getenv("SQLALCHEMY_DATABASE_URI"):
-  engine = create_engine(os.getenv("SQLALCHEMY_DATABASE_URI"))
-else:
-  engine = create_engine('sqlite:///')
+class DataBase:
+  def __init__(self):
+    # Load Database
+    if os.getenv("SQLALCHEMY_DATABASE_URI"):
+      self.engine = create_engine(os.getenv("SQLALCHEMY_DATABASE_URI"))
+    else:
+      self.engine = create_engine('sqlite:///')
 
-# Start a session
-Session = sessionmaker(bind=engine)
-session = Session()
+    self.conn = self.engine.connect()
+    self.Session = sessionmaker(bind=self.engine)
+    self.session = self.Session()
 
-"""
-************************* Scrape data to render the map from ************************* 
+    # self.stocked_lakes = self.conn.execute(text("SELECT * FROM stocked_lakes_table")).fetchall()
+    # print(self.stocked_lakes)
+    # self.derby_lakes = self.conn.execute(text("SELECT * FROM derby_lakes_table")).fetchall()
+    # self.total_stocked_by_date = self.session.query(
+    #   StockedLakes.date,
+    #   func.sum(StockedLakes.stocked_fish)
+    # ).group_by('date').order_by('date').all()
 
-Need: Lake names, stock count, date stocked, derby participant, and lat/lon
-Steps: 
-1. Scrape the data from wdfw.wa.gov
-2. Use Geolocator to get lat/lon
-3. Make the data into a list of dictionaries 
-4. Save data to Postgres Database 
+  def get_data(self):
+    stocked_lakes = self.conn.execute(text("SELECT * FROM stocked_lakes_table")).fetchall()
+    derby_lakes = self.conn.execute(text("SELECT * FROM derby_lakes_table")).fetchall()
+    total_stocked_by_date = self.session.query(
+      StockedLakes.date,
+      func.sum(StockedLakes.stocked_fish)
+    ).group_by(StockedLakes.date).order_by(StockedLakes.date).all()
+    return {"stocked_lakes": stocked_lakes, "derby_lakes": derby_lakes, "total_stocked_by_date": total_stocked_by_date}
 
-"""
+  def write_data(self):
+    scraper = Scraper()
+    Base.metadata.drop_all(self.engine)  # TODO: Remove in production
+    Base.metadata.create_all(self.engine)  # TODO: Remove in production
+    self.write_derby_data(scraper)
+    self.write_lake_data(scraper)
+    self.session.commit()
+    self.session.close()
 
-# ARCHIVE
-# lake_url = "https://wdfw.wa.gov/fishing/reports/stocking/trout-plants/archive/2021?lake_stocked=&county=&species=&hatchery=&region=&items_per_page=250&page=4" # ARCHIVE
-# ARCHIVE
+  def write_derby_data(self, scraper):
+    derby_lakes = scraper.derby_lake_names
+    for lake in derby_lakes:
+      for item in scraper.df:
+        if lake.capitalize() in item['lake'].capitalize():
+          item['derby_participant'] = True
+          self.session.add(DerbyLake(lake=lake))
 
+  def write_lake_data(self, scraper):
+    for lake_data in scraper.df:
+      # check if entry already exists in the table
+      existing_lake = self.session.query(StockedLakes).filter_by(lake=lake_data['lake'],
+                                                                 stocked_fish=lake_data['stocked_fish'],
+                                                                 date=lake_data['date']).first()
+      if existing_lake:
+        continue  # skip if the lake already exists in the table
 
-lake_url = "https://wdfw.wa.gov/fishing/reports/stocking/trout-plants/all?lake_stocked=&county=&species=&hatchery=&region=&items_per_page=250"
-response = requests.get(lake_url)
-if response.status_code != 200:
-  print("Error fetching page")
-  exit()
-soup = BeautifulSoup(response.content, "html.parser")
+      # add the lake to the table if it doesn't exist
+      lake = StockedLakes(lake=lake_data['lake'], stocked_fish=lake_data['stocked_fish'], date=lake_data['date'],
+                          latitude=lake_data['latitude'], longitude=lake_data['longitude'],
+                          directions=lake_data['directions'], derby_participant=lake_data['derby_participant'])
 
-
-# return list of lake names
-def scrape_lake_names():
-  found_text = soup.findAll(class_="views-field views-field-lake-stocked")
-
-  text_list = [i.text + " County, Washington" for i in found_text]
-
-  # Clean up Names
-  text_lst_trimmed = []
-  for i in text_list:
-    text_lst_trimmed.append(i.replace("\n", "").replace("- Region", ''))
-  text_lst_trimmed = [re.sub(r"\(.*?\)", '', text) for text in text_lst_trimmed]
-  text_lst_trimmed = [re.sub(r'[^\w\s]', '', text) for text in text_lst_trimmed]
-  text_lst_trimmed = [re.sub(r"\d", '', text) for text in text_lst_trimmed]
-  text_lst_trimmed = [re.sub(r"\bLK\b", 'Lake', text) for text in text_lst_trimmed]
-  text_lst_trimmed = [re.sub(r"\bPD\b", 'Pond', text) for text in text_lst_trimmed]
-  text_lst_trimmed = [re.sub(r"\bCR\b", 'Creek', text) for text in text_lst_trimmed]
-  text_lst_trimmed = [re.sub(r"\bPRK\b", 'Park', text) for text in text_lst_trimmed]
-  text_lst_trimmed = [re.sub(r"\bCO\b", 'County', text) for text in text_lst_trimmed]
-  text_lst_trimmed = [" ".join(text.split()) for text in text_lst_trimmed]
-
-  return text_lst_trimmed
-
-
-# return list of Stock Counts
-def scrape_stock_count():
-  found_stock_counts = soup.findAll(class_="views-field views-field-num-fish")
-
-  found_stock_text_list = [i.text.strip() for i in found_stock_counts]
-
-  return found_stock_text_list
+      self.session.add(lake)
 
 
-# Return list of Scraped Dates
-def scrape_date():
-  date_text = soup.findAll(class_="views-field views-field-stock-date")
+class Scraper:
+  """
+  ************************* Scrape data to render the map from *************************
 
-  date_text_list = [i.text.strip() for i in date_text]
+  Need: Lake names, stock count, date stocked, derby participant, and lat/lon
+  Steps:
+  1. Scrape the data from wdfw.wa.gov
+  2. Use Geolocator to get lat/lon
+  3. Make the data into a list of dictionaries
+  """
 
-  return date_text_list
+  def __init__(self):
+    self.lake_url = "https://wdfw.wa.gov/fishing/reports/stocking/trout-plants/all?lake_stocked=&county=&species=&hatchery=&region=&items_per_page=250"
+    self.response = requests.get(self.lake_url)
+    if self.response.status_code != 200:
+      print("Error fetching page")
+      exit()
+    self.soup = BeautifulSoup(self.response.content, "html.parser")
+    self.lake_names = self.scrape_lake_names()
+    self.stock_counts = self.scrape_stock_count()
+    self.dates = self.scrape_date()
+    self.df = self.make_df()
+    self.derby_lake_names = self.scrape_derby_names()
 
+  def scrape_lake_names(self):
+    ABBREVIATIONS = {
+      "LK": "Lake",
+      "PD": "Pond",
+      "CR": "Creek",
+      "PRK": "Park",
+      "CO": "County"
+    }
 
-# Get the names of lakes that are in the state trout derby
-def scrape_derby_names():
-  global response, soup
-  url_string = "https://wdfw.wa.gov/fishing/contests/trout-derby/lakes"
-  response = requests.get(url_string)
-  if response.status_code != 200:
-    print("Error fetching page")
-    exit()
-  soup = BeautifulSoup(response.content, "html.parser")
+    found_text = self.soup.select('.views-field-lake-stocked')
 
-  # Scrape Names
-  text_list = []
-  found_text = soup.find("div", {"class": "derby-lakes-list"}).findAll("ul", recursive=False)
+    # Clean up the names
+    text_list = [re.sub(r"\(.*?\)|[^\w\s\d]|(?<!\w)(\d+)(?!\w)|\b(" + "|".join(ABBREVIATIONS.keys()) + r")\b",
+                        lambda match: "" if match.group(1) else ABBREVIATIONS.get(match.group(2), ""),
+                        i.text.strip() + " County").strip().replace("\n", "").replace(" Region ", '').replace("  ",
+                                                                                                              " ").title()
+                 for i in found_text]
+    return text_list
 
-  for i in found_text:
-    text_list.append(i.find("li").text)
+  # return list of Stock Counts
+  def scrape_stock_count(self):
+    found_stock_counts = self.soup.findAll(class_="views-field views-field-num-fish")
 
-  # Clean up Names
-  text_lst_trimmed = []
-  for i in text_list:
-    text_lst_trimmed.append(i.replace("\n", ""))
-  text_lst_trimmed = [re.sub(r"\(.*?\)", '', text) for text in text_lst_trimmed]
-  return text_lst_trimmed
+    stock_count_text_list = [i.text.strip().replace(',', '') for i in found_stock_counts]
+    stock_count_int_list = []
+    for i in stock_count_text_list:
+      try:
+        stock_count_int_list.append(int(i))
+      except ValueError:
+        stock_count_int_list.append(i)
+        print(f"Error: {i} is not a valid number")
+        continue
 
+    return stock_count_int_list
 
-def get_lat_lon(data):
-  locator = GoogleV3(api_key=os.getenv('GV3_API_KEY'))
+  # Return list of Scraped Dates
+  def scrape_date(self):
+    date_text = self.soup.findAll(class_="views-field views-field-stock-date")
 
-  for i in range(len(data)):
-    if data[i]['lake']:
-      geocode = locator.geocode(data[i]['lake'])
-      if geocode:
-        data[i]['latitude'] = geocode.point[0]
-        data[i]['longitude'] = geocode.point[1]
-        data[i]['directions'] = f"https://www.google.com/maps/search/?api=1&query={data[i]['lake']}"
-      else:
-        data[i]['latitude'] = None
-        data[i]['longitude'] = None
-        data[i]['directions'] = f"https://www.google.com/maps/search/?api=1&query={data[i]['lake']}"
+    date_text_list = [i.text.strip() for i in date_text]
+    date_list = []
+    for i in date_text_list:
+      try:
 
-  return data
+        date_list.append(datetime.strptime(i, '%b %d, %Y') or datetime.strptime(i, '%b %dd, %Y'))
+      except ValueError:
+        date_list.append(i)
+        print(f"Error: {i} is not a valid number")
+        continue
 
+    return date_list
 
-def write_derby_data(data):
-  derby_lakes = scrape_derby_names()
-  for lake in derby_lakes:
-    for item in data:
-      if lake.capitalize() in item['lake'].capitalize():
-        item['derby_participant'] = True
-        session.add(DerbyLake(lake=lake))
-  session.commit()
-  return data
+  def make_df(self):
+    # lakes = scrape_lake_names()
+    amount_scraped = len(self.lake_names)
+    stock_count = self.stock_counts[1:amount_scraped]
+    dates = self.dates[1:amount_scraped]
 
+    # Create a list of dictionaries
+    data = []
+    for i in range(amount_scraped - 1):
+      data.append(
+        {'lake': self.lake_names[i], 'stocked_fish': stock_count[i], 'date': dates[i], 'latitude': "", 'longitude': "",
+         'directions': "", "derby_participant": False})
 
-def write_lake_data(data):
-  for lake_data in data:
-    # check if entry already exists in the table
-    existing_lake = session.query(StockedLakes).filter_by(lake=lake_data['lake'],
-                                                          stocked_fish=lake_data['stocked_fish'],
-                                                          date=lake_data['date']).first()
-    if existing_lake:
-      continue  # skip if the lake already exists in the table
+    data = self.get_lat_lon(data)  # ? side effect
 
-    # add the lake to the table if it doesn't exist
-    lake = StockedLakes(lake=lake_data['lake'], stocked_fish=lake_data['stocked_fish'], date=lake_data['date'],
-                        latitude=lake_data['latitude'], longitude=lake_data['longitude'],
-                        directions=lake_data['directions'], derby_participant=lake_data['derby_participant'])
+    return data
 
-    session.add(lake)
+  # Get the latitude and longitude of the lake names and update the df
+  @staticmethod
+  def get_lat_lon(data):
+    locator = GoogleV3(api_key=os.getenv('GV3_API_KEY'))
 
-  session.commit()
+    for i in range(len(data)):
+      lake = data[i]['lake']
+      if lake:
+        geocode = locator.geocode(lake + ' washington state')
+        if geocode:
+          data[i]['latitude'] = geocode.point[0]
+          data[i]['longitude'] = geocode.point[1]
+          data[i]['directions'] = f"https://www.google.com/maps/search/?api=1&query={lake}"
+        else:
+          data[i]['latitude'] = ''
+          data[i]['longitude'] = ''
+          data[i]['directions'] = f"https://www.google.com/maps/search/?api=1&query={lake}"
+    print(data)
+    return data
 
+  # Get the names of lakes that are in the state trout derby
+  def scrape_derby_names(self):
+    url_string = "https://wdfw.wa.gov/fishing/contests/trout-derby/lakes"
 
-def make_df():
-  lakes = scrape_lake_names()
-  amount_scraped = len(lakes)
-  lakes = lakes[1:amount_scraped]
+    # Reassign response and soup to new url
+    self.response = requests.get(url_string)
+    self.soup = BeautifulSoup(self.response.content, "html.parser")
 
-  stock_count = scrape_stock_count()
-  stock_count = stock_count[1:amount_scraped]
+    # Scrape Names
+    text_list = []
+    found_text = self.soup.find("div", {"class": "derby-lakes-list"}).findAll("ul", recursive=False)
 
-  dates = scrape_date()
-  dates = dates[1:amount_scraped]
+    for i in found_text:
+      text_list.append(i.find("li").text)
 
-  # Create a list of dictionaries
-  data = []
-  for i in range(amount_scraped - 1):
-    data.append({'lake': lakes[i], 'stocked_fish': stock_count[i], 'date': dates[i], 'latitude': "", 'longitude': "",
-                 'directions': "", "derby_participant": False})
-  data = get_lat_lon(data)
-  Base.metadata.drop_all(engine)
-  Base.metadata.create_all(engine)
-
-  # Write the data
-  new_data = write_derby_data(data)
-  write_lake_data(new_data)
-  session.close()
+    # Clean up Names
+    text_lst_trimmed = []
+    for i in text_list:
+      text_lst_trimmed.append(i.replace("\n", ""))
+    text_lst_trimmed = [re.sub(r"\(.*?\)", '', text).title() for text in text_lst_trimmed]
+    return text_lst_trimmed
 
 
 # Run Once Every morning on Heroku Scheduler
 if __name__ == "__main__":
   start_time = time.time()
 
-  make_df()
+  data_base = DataBase()
+  # Scraper()
+  data_base.write_data()
 
   end_time = time.time()
   print(f"It took {end_time - start_time:.2f} seconds to compute")
