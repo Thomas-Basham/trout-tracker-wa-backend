aa8a917548d3001f002a49a74c435fa8c22ac117
Thomas Basham <Bashamtg@gmail.com>
2023-05-19 14:03:34 -0700

    added jsonify import

--- front_end/main.py
+++ front_end/main.py
@@ -1,5 +1,5 @@
 from dotenv import load_dotenv
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from data_base import DataBase
 from map_and_charts import make_map, show_total_stocked_by_date_chart, show_total_stocked_by_hatchery_chart
 from time import time
