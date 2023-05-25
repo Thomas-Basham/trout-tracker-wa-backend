b96be0c92e5bb68b61b2b807aa3886857a67bdc1
Thomas Basham <Bashamtg@gmail.com>
2023-05-25 10:16:17 -0700

    updated to run app from root directory

--- front_end/wsgi.py
+++ front_end/wsgi.py
@@ -1,11 +1,11 @@
-from main import app
+from front_end.main import app
 import os
 from werkzeug.serving import run_simple
 
 if __name__ == "__main__":
 
   os.environ['FLASK_APP'] = 'wsgi.py'
-  
+
   if os.getenv('ENVIRONMENT') and os.getenv('ENVIRONMENT') != 'production':
     print('TESTING ENVIRONMENT')
 
