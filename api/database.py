748c1b1f9fb5519be258059a8515720125d854aa
Thomas Basham <Bashamtg@gmail.com>
2024-01-26 11:22:56 -0800

    organized code

--- api/database.py
+++ api/database.py
@@ -13,192 +13,198 @@ Base = declarative_base()
 
 # Create the stocked_lakes_table class
 class StockedLakes(Base):
-  __tablename__ = 'stocked_lakes_table'
-  id = Column(Integer, primary_key=True)
-  lake = Column(String)
-  stocked_fish = Column(Integer)
-  species = Column(String)
-  weight = Column(Float)
-  hatchery = Column(String)
-  date = Column(Date)
-  latitude = Column(Float)
-  longitude = Column(Float)
-  directions = Column(String)
-  derby_participant = Column(Boolean)
+    __tablename__ = 'stocked_lakes_table'
+    id = Column(Integer, primary_key=True)
+    lake = Column(String)
+    stocked_fish = Column(Integer)
+    species = Column(String)
+    weight = Column(Float)
+    hatchery = Column(String)
+    date = Column(Date)
+    latitude = Column(Float)
+    longitude = Column(Float)
+    directions = Column(String)
+    derby_participant = Column(Boolean)
 
-  def to_dict(self):
-      return {
-          "date": self.date,
-          "lake": self.lake,
-          "stocked_fish": self.stocked_fish,
-          "species": self.species,
-          "hatchery": self.hatchery,
-          "weight": self.weight,
-          "latitude": self.latitude,
-          "longitude": self.longitude,
-          "directions": self.directions,
-          "derby_participant": self.derby_participant
-      }
+    def to_dict(self):
+        return {
+            "date": self.date,
+            "lake": self.lake,
+            "stocked_fish": self.stocked_fish,
+            "species": self.species,
+            "hatchery": self.hatchery,
+            "weight": self.weight,
+            "latitude": self.latitude,
+            "longitude": self.longitude,
+            "directions": self.directions,
+            "derby_participant": self.derby_participant
+        }
 
 
 # Create the derby_lakes_table class
 class DerbyLake(Base):
-  __tablename__ = 'derby_lakes_table'
-  id = Column(Integer, primary_key=True)
-  lake = Column(String)
+    __tablename__ = 'derby_lakes_table'
+    id = Column(Integer, primary_key=True)
+    lake = Column(String)
 
 
 # Create the derby_lakes_table class
 class Utility(Base):
-  __tablename__ = 'utility_table'
-  id = Column(Integer, primary_key=True)
-  updated = Column(Date)
+    __tablename__ = 'utility_table'
+    id = Column(Integer, primary_key=True)
+    updated = Column(Date)
 
 
 class DataBase:
-  def __init__(self):
-    # Load Database
-    if os.getenv("SQLALCHEMY_DATABASE_URI"):
-      self.engine = create_engine(os.getenv("SQLALCHEMY_DATABASE_URI"))
-    else:
-      print("USING SQLITE DB")
-      self.engine = create_engine('sqlite:///api/sqlite.db', connect_args={"check_same_thread": False},)
+    def __init__(self):
+        # Load Database
+        if os.getenv("SQLALCHEMY_DATABASE_URI"):
+            self.engine = create_engine(os.getenv("SQLALCHEMY_DATABASE_URI"))
+        else:
+            print("USING SQLITE DB")
+            self.engine = create_engine(
+                'sqlite:///api/sqlite.db', connect_args={"check_same_thread": False},)
 
-    self.conn = self.engine.connect()
-    self.Session = sessionmaker(bind=self.engine)
-    self.session = self.Session()
+        self.conn = self.engine.connect()
+        self.Session = sessionmaker(bind=self.engine)
+        self.session = self.Session()
 
-    self.insert_counter = 0
+        self.insert_counter = 0
 
-  def get_stocked_lakes_data(self, end_date = datetime.now(), start_date = datetime.now() - timedelta(days=7) ):
-      
-      query = """
-      SELECT date, lake, stocked_fish, species, hatchery, weight, latitude, longitude, directions, derby_participant
-      FROM stocked_lakes_table
-      WHERE date BETWEEN :start_date AND :end_date
-      ORDER BY date
-      """
-      
-      # stocked_lakes = self.conn.execute(text(query), parameters=dict(start_date=start_date, end_date=end_date)).fetchall()
+    def get_stocked_lakes_data(self, end_date=datetime.now(), start_date=datetime.now() - timedelta(days=7)):
 
-    # Query the StockedLake model
-      stocked_lakes = self.session.query(StockedLakes) \
-        .filter(StockedLakes.date.between(start_date, end_date)) \
-        .order_by(StockedLakes.date.desc()) \
-        .all()
 
-      # self.session.close()
-      return stocked_lakes
-  
 
-  def get_hatchery_totals(self, end_date = datetime.now(), start_date = datetime.now() - timedelta(days=7)):
-      
-      query = """
+        # Query the StockedLake model
+        stocked_lakes = self.session.query(StockedLakes) \
+            .filter(StockedLakes.date.between(start_date, end_date)) \
+            .order_by(StockedLakes.date.desc()) \
+            .all()
+
+        # self.session.close()
+        return stocked_lakes
+
+    def get_hatchery_totals(self, end_date=datetime.now(), start_date=datetime.now() - timedelta(days=7)):
+
+        query = """
       SELECT hatchery, SUM(stocked_fish) AS sum_1
       FROM stocked_lakes_table
       WHERE date BETWEEN :start_date AND :end_date
       GROUP BY hatchery
       ORDER BY sum_1 DESC
       """
-      
-      hatchery_totals = self.conn.execute(text(query),  start_date=start_date, end_date=end_date).fetchall()
-      print("hatchery_totals", hatchery_totals)
-      return hatchery_totals
 
-  def get_total_stocked_by_date_data(self,  end_date = datetime.now(), start_date = datetime.now() - timedelta(days=7)):
-    
-    query = """
-    SELECT date, SUM(stocked_fish) AS sum_1
-    FROM stocked_lakes_table
-    WHERE date BETWEEN :start_date AND :end_date
-    GROUP BY date
-    ORDER BY date
-    """
-    
-    total_stocked_by_date = self.conn.execute(text(query),  start_date= start_date, end_date= end_date).fetchall()
+        hatchery_totals = self.conn.execute(
+            text(query),  start_date=start_date, end_date=end_date).fetchall()
+        print("hatchery_totals", hatchery_totals)
+        return hatchery_totals
 
-    if str(self.engine) == "Engine(sqlite:///api/sqlite.db)":
-      total_stocked_by_date = [(datetime.strptime(date_str, "%Y-%m-%d"), stocked_fish) for date_str, stocked_fish in total_stocked_by_date]
-    
-    return total_stocked_by_date
+    def get_total_stocked_by_date_data(self,  end_date=datetime.now(), start_date=datetime.now() - timedelta(days=7)):
 
-  def get_derby_lakes_data(self):
-    query = "SELECT * FROM derby_lakes_table"
-    derby_lakes = self.conn.execute(text(query)).fetchall()
-    return derby_lakes
+        query = """
+        SELECT date, SUM(stocked_fish) AS sum_1
+        FROM stocked_lakes_table
+        WHERE date BETWEEN :start_date AND :end_date
+        GROUP BY date
+        ORDER BY date
+        """
 
-  def get_date_data_updated(self):
-    query = "SELECT updated FROM utility_table ORDER BY id DESC LIMIT 1"
-    last_updated = self.conn.execute(text(query)).scalar()
-    return last_updated
-  
-  def write_data(self, scraper):
-    if str(self.engine) == "Engine(sqlite:///api/sqlite.db)":
-        Base.metadata.drop_all(self.engine)
-        Base.metadata.create_all(self.engine)
-        
-    self.write_derby_data(scraper)
-    self.write_lake_data(scraper)
-    self.write_utility_data()
-    self.session.commit()
-    self.session.close()
+        total_stocked_by_date = self.conn.execute(
+            text(query),  start_date=start_date, end_date=end_date).fetchall()
 
-  def write_derby_data(self, scraper):
-    derby_lakes = scraper.derby_lake_names
-    for lake in derby_lakes:
-      for item in scraper.df:
-        if lake.capitalize() in item['lake'].capitalize():
-          item['derby_participant'] = True
-          existing_lake = self.session.query(DerbyLake).filter_by(lake=lake).first()
-          if existing_lake:
-            continue  # skip if the lake already exists in the table
-          self.session.add(DerbyLake(lake=lake))
+        if str(self.engine) == "Engine(sqlite:///api/sqlite.db)":
+            total_stocked_by_date = [(datetime.strptime(date_str, "%Y-%m-%d"), stocked_fish)
+                                     for date_str, stocked_fish in total_stocked_by_date]
 
-  def write_lake_data(self, scraper):
-    for lake_data in scraper.df:
-      # check if entry already exists in the table
-      existing_lake = self.session.query(StockedLakes).filter_by(lake=lake_data['lake'],
-                                                                 stocked_fish=lake_data['stocked_fish'],
-                                                                 date=lake_data['date']).first()
-      if existing_lake:
-        continue  # skip if the lake already exists in the table
+        return total_stocked_by_date
 
-      # add the lake to the table if it doesn't exist
-      lake = StockedLakes(lake=lake_data['lake'], stocked_fish=lake_data['stocked_fish'], date=lake_data['date'],
-                          latitude=lake_data['latitude'], longitude=lake_data['longitude'],
-                          directions=lake_data['directions'], derby_participant=lake_data['derby_participant'],
-                          weight=lake_data['weight'], species=lake_data['species'], hatchery=lake_data['hatchery'])
+    def get_derby_lakes_data(self):
+        query = "SELECT * FROM derby_lakes_table"
+        derby_lakes = self.conn.execute(text(query)).fetchall()
+        return derby_lakes
 
-      self.session.add(lake)
+    def get_date_data_updated(self):
+        query = "SELECT updated FROM utility_table ORDER BY id DESC LIMIT 1"
+        last_updated = self.conn.execute(text(query)).scalar()
+        return last_updated
 
-      self.insert_counter += 1
+    def write_data(self, scraper):
+        if str(self.engine) == "Engine(sqlite:///api/sqlite.db)":
+            Base.metadata.drop_all(self.engine)
+            Base.metadata.create_all(self.engine)
 
-    print(f'There were {self.insert_counter} entries added to {str(StockedLakes.__tablename__)}')
+        self.write_derby_data(scraper)
+        self.write_lake_data(scraper)
+        self.write_utility_data()
+        self.session.commit()
+        self.session.close()
 
-  def write_utility_data(self):
-    self.session.add(Utility(updated=datetime.now().date()))
+    def write_derby_data(self, scraper):
+        derby_lakes = scraper.derby_lake_names
+        if derby_lakes:
+            for lake in derby_lakes:
+                for item in scraper.df:
+                    if lake.capitalize() in item['lake'].capitalize():
+                        item['derby_participant'] = True
+                        existing_lake = self.session.query(
+                            DerbyLake).filter_by(lake=lake).first()
+                        if existing_lake:
+                            continue  # skip if the lake already exists in the table
+                        self.session.add(DerbyLake(lake=lake))
+        else:
+            # If there are no derby lakes, clear all derby participants from Stocked Lake table, delete all derby table entries
+            self.session.query(DerbyLake).delete()
 
-  def back_up_database(self):
-    all_stocked_lakes = self.session.query(StockedLakes).all()
+            lakes_to_update = self.session.query(StockedLakes).filter_by(derby_participant=True)
+            for lake in lakes_to_update:
+                lake.derby_participant = False
 
-    backup_file_txt = 'web_scraper/backup_data.txt'
-    backup_file_sql = 'web_scraper/backup_data.sql'
+    def write_lake_data(self, scraper):
+        for lake_data in scraper.df:
+            # check if entry already exists in the table
+            existing_lake = self.session.query(StockedLakes).filter_by(lake=lake_data['lake'],
+                                                                       stocked_fish=lake_data['stocked_fish'],
+                                                                       date=lake_data['date']).first()
+            if existing_lake:
+                continue  # skip if the lake already exists in the table
 
-    if os.path.exists(backup_file_txt):
-      os.remove(backup_file_txt)
-    if os.path.exists(backup_file_sql):
-      os.remove(backup_file_sql)
+            # add the lake to the table if it doesn't exist
+            lake = StockedLakes(lake=lake_data['lake'], stocked_fish=lake_data['stocked_fish'], date=lake_data['date'],
+                                latitude=lake_data['latitude'], longitude=lake_data['longitude'],
+                                directions=lake_data['directions'], derby_participant=lake_data['derby_participant'],
+                                weight=lake_data['weight'], species=lake_data['species'], hatchery=lake_data['hatchery'])
 
-    with open(backup_file_txt, 'w') as f:
-      for row in all_stocked_lakes:
-        # Write each column value separated by a comma
-        f.write(
-          f"{row.id},{row.lake},{row.stocked_fish},{row.species},{row.weight},{row.hatchery},{row.date},{row.latitude},{row.longitude},{row.directions},{row.derby_participant}\n")
+            self.session.add(lake)
 
-    with open(backup_file_sql, 'w') as f:
-      for row in all_stocked_lakes:
-        # Write an INSERT INTO statement for each row
-        f.write(
-          f"INSERT INTO stocked_lakes_table (id, lake, stocked_fish, species, weight, hatchery, date, latitude, longitude, directions, derby_participant) VALUES ({row.id}, '{row.lake}', {row.stocked_fish}, '{row.species}', {row.weight}, '{row.hatchery}', '{row.date}', '{row.latitude}', '{row.longitude}', '{row.directions}', {row.derby_participant});\n")
+            self.insert_counter += 1
 
-    print(f"Database backed up to {backup_file_txt} and {backup_file_sql}")
+        print(
+            f'There were {self.insert_counter} entries added to {str(StockedLakes.__tablename__)}')
+
+    def write_utility_data(self):
+        self.session.add(Utility(updated=datetime.now().date()))
+
+    def back_up_database(self):
+        all_stocked_lakes = self.session.query(StockedLakes).all()
+
+        backup_file_txt = 'web_scraper/backup_data.txt'
+        backup_file_sql = 'web_scraper/backup_data.sql'
+
+        if os.path.exists(backup_file_txt):
+            os.remove(backup_file_txt)
+        if os.path.exists(backup_file_sql):
+            os.remove(backup_file_sql)
+
+        with open(backup_file_txt, 'w') as f:
+            for row in all_stocked_lakes:
+                # Write each column value separated by a comma
+                f.write(
+                    f"{row.id},{row.lake},{row.stocked_fish},{row.species},{row.weight},{row.hatchery},{row.date},{row.latitude},{row.longitude},{row.directions},{row.derby_participant}\n")
+
+        with open(backup_file_sql, 'w') as f:
+            for row in all_stocked_lakes:
+                # Write an INSERT INTO statement for each row
+                f.write(
+                    f"INSERT INTO stocked_lakes_table (id, lake, stocked_fish, species, weight, hatchery, date, latitude, longitude, directions, derby_participant) VALUES ({row.id}, '{row.lake}', {row.stocked_fish}, '{row.species}', {row.weight}, '{row.hatchery}', '{row.date}', '{row.latitude}', '{row.longitude}', '{row.directions}', {row.derby_participant});\n")
+
+        print(f"Database backed up to {backup_file_txt} and {backup_file_sql}")
--- api/main.py
+++ api/main.py
@@ -1,10 +1,9 @@
 # main.py
 from dotenv import load_dotenv
-from flask import Flask,request, jsonify
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 from api.database import DataBase
 from datetime import datetime, timedelta
-from dateutil import parser
 
 load_dotenv()
 app = Flask(__name__.split('.')[0])
@@ -15,6 +14,7 @@ CORS(app)
 # CORS(app, origins=["http://localhost:3000"])
 CORS(app, resources={r"/*": {"origins": "*"}})  # Allows all domains
 db = DataBase()
+
 
 @app.route('/', methods=['GET'])
 def index_view():
@@ -67,60 +67,66 @@ def index_view():
     }
     return jsonify(api_info)
 
-# Route for retrieving stocked lakes data
+
 @app.route('/stocked_lakes_data', methods=['GET'])
 def get_stocked_lakes_data():
     now = datetime.now()
 
     end_date = request.args.get('end_date', default=now)
-    parsed_end_date = parser.parse(end_date, tzinfos={"Z": 0})
-    start_date = request.args.get('start_date', default= now - timedelta(days=7))
-    parsed_start_date = parser.parse(start_date, tzinfos={"Z": 0})
-    
+
+    start_date = request.args.get(
+        'start_date', default=now - timedelta(days=7))
+
     stocked_lakes = db.get_stocked_lakes_data(start_date, end_date)
-    stocked_lakes_dicts = [stocked_lake.to_dict() for stocked_lake in stocked_lakes]
-    print(stocked_lakes_dicts)
+    stocked_lakes_dicts = [stocked_lake.to_dict()
+                           for stocked_lake in stocked_lakes]
+
     return jsonify(stocked_lakes_dicts)
 
-# Route for retrieving total stocked by date data
+
 @app.route('/total_stocked_by_date_data', methods=['GET'])
 def get_total_stocked_by_date_data():
     now = datetime.now()
 
     end_date = request.args.get('end_date', default=now)
-    parsed_end_date = parser.parse(end_date, tzinfos={"Z": 0})
-    start_date = request.args.get('start_date', default= now - timedelta(days=7))
-    parsed_start_date = parser.parse(start_date, tzinfos={"Z": 0})
-    
-    total_stocked_by_date = db.get_total_stocked_by_date_data(start_date, end_date)
-    total_stocked_by_date = [(str(date), stocked_fish) for date, stocked_fish in total_stocked_by_date]
+
+    start_date = request.args.get(
+        'start_date', default=now - timedelta(days=7))
+
+    total_stocked_by_date = db.get_total_stocked_by_date_data(
+        start_date, end_date)
+
+    # Convert to a list of dictionaries
+    total_stocked_by_date = [{"date": str(date), "stocked_fish": stocked_fish}
+                             for date, stocked_fish in total_stocked_by_date]
+
     return jsonify(total_stocked_by_date)
 
-# Route for retrieving hatchery totals
+
 @app.route('/hatchery_totals', methods=['GET'])
 def get_hatchery_totals():
     now = datetime.now()
 
     end_date = request.args.get('end_date', default=now)
-    parsed_end_date = parser.parse(end_date, tzinfos={"Z": 0})
-    start_date = request.args.get('start_date', default= now - timedelta(days=7))
-    parsed_start_date = parser.parse(start_date, tzinfos={"Z": 0})
-    
+
+    start_date = request.args.get(
+        'start_date', default=now - timedelta(days=7))
+
     hatchery_totals = db.get_hatchery_totals(start_date, end_date)
-    hatchery_totals = [{'hatchery': row[0], 'sum_1': row[1]} for row in hatchery_totals]
+    hatchery_totals = [{'hatchery': row[0], 'sum_1': row[1]}
+                       for row in hatchery_totals]
     return jsonify(hatchery_totals)
 
-# Route for retrieving derby lakes data
+
 @app.route('/derby_lakes_data', methods=['GET'])
 def get_derby_lakes_data():
     derby_lakes = db.get_derby_lakes_data()
     derby_lakes = [dict(row) for row in derby_lakes]
     return jsonify(derby_lakes)
 
-# Route for retrieving the date data was last updated
+
 @app.route('/date_data_updated', methods=['GET'])
 def get_date_data_updated():
     last_updated = db.get_date_data_updated()
     last_updated = str(last_updated)
     return jsonify(last_updated)
-
